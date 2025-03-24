e3361f9b68c5dce090818ee332c3a64078d04e82
Maroka-chan <64618598+Maroka-chan@users.noreply.github.com>
2025-03-24 21:22:59 +0100

    initial commit

---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    glibc.url = "github:nixos/nixpkgs/3b05df1d13c1b315cecc610a2f3180f6669442f0";
+  };
+
+  outputs = inputs @ { self, nixpkgs, ... }: let
+    pkgs = import nixpkgs { system = "x86_64-linux"; config.allowUnfree = true; };
+    pkgs2 = import inputs.glibc { system = "x86_64-linux"; config.allowUnfree = true; };
+  in {
+    packages.x86_64-linux.default = pkgs.callPackage ./nvhpc.nix { glibc = pkgs2.glibc; libgcc = pkgs2.gcc10.cc.lib; stdenv = pkgs.gcc10Stdenv; };
+    packages.x86_64-linux.nvhpcStdenv = pkgs.callPackage ./nvhpcStdenv.nix {};
+    devShells.x86_64-linux.default = (pkgs.mkShell.override { stdenv = pkgs.gcc10Stdenv; }) {
+      langFortran = true;
+      nativeBuildInputs = with pkgs; [
+        pkg-config
+      ];
+      buildInputs = with pkgs; [
+        meson
+        ninja
+        lapack
+        lapack.dev
+        openblasCompat
+        openblasCompat.dev
+        python3
+        mkl
+        cmake
+        asciidoctor
+      ];
+
+      shellHook = let
+        nvhome = self.packages.x86_64-linux.default;
+        target = "Linux_x86_64";
+        version = "21.1";
+        nvcudadir = "${nvhome}/${target}/${version}/cuda";
+        nvcompdir = "${nvhome}/${target}/${version}/compilers";
+        nvmathdir = "${nvhome}/${target}/${version}/math_libs";
+        nvcommdir = "${nvhome}/${target}/${version}/comm_libs";
+      in ''
+        export NVHPC=${nvhome}
+        export CC=${nvcompdir}/bin/nvc
+        export CXX=${nvcompdir}/bin/nvc++
+        export FC=${nvcompdir}/bin/nvfortran
+        export F90=${nvcompdir}/bin/nvfortran
+        export F77=${nvcompdir}/bin/nvfortran
+        export CPP=cpp
+
+        export PATH=${nvcommdir}/mpi/bin:${nvcompdir}/bin:${nvcudadir}/bin:$PATH
+        export LD_LIBRARY_PATH=${nvcommdir}/nvshmem/lib:${nvcommdir}/nccl/lib:${nvcommdir}/mpi/lib:${nvmathdir}/lib64:${nvcompdir}/lib:${nvcudadir}/extras/CUPTI/lib64:${nvcudadir}/lib64:$LD_LIBRARY_PATH
+        export CPATH=${nvcommdir}/nvshmem/include:${nvcommdir}/nccl/include:${nvcommdir}/mpi/include:${nvmathdir}/include:$CPATH
+        export MANPATH=${nvcompdir}/man:$MANPATH
+        export OPAL_PREFIX=${nvcommdir}/mpi
+      '';
+    };
+  };
+}
---
+++ nvhpc.nix
@@ -0,0 +1,63 @@
+{
+  stdenv,
+  fetchzip,
+  autoPatchelfHook,
+  flock,
+  libgcc,
+  libz,
+  zstd,
+  libxml2,
+  glibc,
+  bc,
+  #gcc10
+}:
+stdenv.mkDerivation rec {
+  name = "nvhpc";
+  version = "21.1";
+  src = fetchzip {
+    url = "https://developer.download.nvidia.com/hpc-sdk/21.1/nvhpc_2021_211_Linux_x86_64_cuda_11.2.tar.gz";
+    hash = "sha256-e1h8vyb7grhIEWBUfFQcsfMSHlqszgBWJQq14pr4rvY=";
+  };
+
+  dontConfigure = true;
+  #dontPatch = true;
+  dontBuild = true;
+  dontFixup = true;
+
+  nativeBuildInputs = [
+    autoPatchelfHook
+    flock
+    #gcc10
+  ];
+
+  buildInputs = [
+    bc
+  ];
+
+  patchPhase = ''
+    find ./install_components/Linux_x86_64/${version} -type f -executable -exec file {} + | awk -F: '/dynamically linked/ && !/shared object/ {print $1}' \
+      | xargs -I{} patchelf --set-interpreter "$(cat "$NIX_CC"/nix-support/dynamic-linker)" --set-rpath "${libgcc.lib}/lib:${libgcc.out}/lib:${libz}/lib:${zstd.out}/lib:${libxml2}/lib" "{}"
+  '';
+
+  installPhase = ''
+    mkdir -p $out
+
+    sed -i 's|/bin/chmod|chmod|g' install_components/install
+    sed -i 's|/sbin/ldconfig|ldconfig|g' install_components/install
+    patchShebangs install_components/Linux_x86_64/${version}/compilers/bin/makelocalrc
+    sed -i '/makelocalrc executed by/d' install_components/Linux_x86_64/${version}/compilers/bin/makelocalrc
+
+    patchShebangs ./install
+    NVHPC_SILENT=true NVHPC_INSTALL_DIR=$out NVHPC_INSTALL_TYPE=single ./install #./install_components/install
+
+    addAutoPatchelfSearchPath $out/Linux_x86_64/${version}/cuda/11.2/targets/x86_64-linux/lib/stubs
+    addAutoPatchelfSearchPath $out/Linux_x86_64/${version}/compilers/lib
+
+    # fix /usr/lib/crt1.o impure path used in link
+    cat >> $out/Linux_x86_64/${version}/compilers/bin/localrc << EOF
+
+    set DEFLIBDIR=${glibc}/lib;
+    set DEFSTDOBJDIR=${glibc}/lib;
+    EOF
+  '';
+}
